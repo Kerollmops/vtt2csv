4e5ba4fe1a0fac7810c4cf23f9126f45189a6f33
Kerollmops <clement@meilisearch.com>
2022-05-19 18:45:32 +0200

    Convert timers into milliseconds timers

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,79 @@ fn main() -> anyhow::Result<()> {
     let mut writer = csv::Writer::from_writer(io::stdout());
     writer.write_record(&["id", "start", "end", "text"])?;
 
-    for content in content.split("\r\n\r\n") {
-        if content.trim().is_empty() {
+    for line in content.split("\r\n\r\n") {
+        if line.trim().is_empty() {
             break;
         }
 
-        let (id, content) = content
-            .split_once("\r\n")
-            .context("invalid line id format")?;
-        let (start_end, text) = content
-            .split_once("\r\n")
-            .context("invalid line start-end format")?;
-        let (start, end) = start_end
-            .split_once(" --> ")
-            .context("invalid line start-end format")?;
-        writer.write_record(&[id, start, end, text])?;
+        let (id, start, end, text) = extract_line_infos(line)?;
+        writer.write_record(&[id, &start.to_string(), &end.to_string(), text])?;
     }
 
     writer.flush()?;
 
     Ok(())
+}
+
+fn extract_line_infos(line: &str) -> anyhow::Result<(&str, u64, u64, &str)> {
+    let (id, line) = line.split_once("\r\n").context("invalid line id format")?;
+    let (start_end, text) = line
+        .split_once("\r\n")
+        .context("invalid line start-end format")?;
+    let (start, end) = start_end
+        .split_once(" --> ")
+        .context("invalid line start-end format")?;
+
+    let start_milliseconds =
+        convert_timer_in_milliseconds(start).context("invalid timer format")?;
+    let end_milliseconds = convert_timer_in_milliseconds(end).context("invalid timer format")?;
+
+    Ok((id, start_milliseconds, end_milliseconds, text))
+}
+
+fn convert_timer_in_milliseconds(timer: &str) -> Option<u64> {
+    let (hours, timer) = timer.split_once(":")?;
+    let (minutes, timer) = timer.split_once(":")?;
+    let (seconds, milliseconds) = timer.split_once(".")?;
+
+    let hours: u64 = hours.parse().ok()?;
+    let minutes: u64 = minutes.parse().ok()?;
+    let seconds: u64 = seconds.parse().ok()?;
+    let milliseconds: u64 = milliseconds.parse().ok()?;
+
+    Some(hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000 + milliseconds)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convertions() {
+        assert_eq!(
+            convert_timer_in_milliseconds("01:09:33.252"),
+            Some(1 * 60 * 60 * 1000 + 9 * 60 * 1000 + 33 * 1000 + 252)
+        );
+        assert_eq!(
+            convert_timer_in_milliseconds("01:09:36.547"),
+            Some(1 * 60 * 60 * 1000 + 9 * 60 * 1000 + 36 * 1000 + 547)
+        );
+    }
+
+    #[test]
+    fn extract_vtt_line_infos() {
+        let line = "1\r\n00:00:20.672 --> 00:00:24.972\r\nEntre l’Australia et la South America,
+dans l’Océan South Pacific…";
+        println!("{:?}", line);
+
+        let (id, start, end, text) = extract_line_infos(line).unwrap();
+        assert_eq!(id, "1");
+        assert_eq!(start, 20672);
+        assert_eq!(end, 24972);
+        assert_eq!(
+            text,
+            "Entre l’Australia et la South America,
+dans l’Océan South Pacific…"
+        );
+    }
 }
