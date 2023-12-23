86229cdd4eebcc5ddaf16e3cf8a9d13b6680416c
Clément Renault <clement@meilisearch.com>
2023-12-23 12:41:39 +0100

    Improve errors

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,16 @@ fn main() -> anyhow::Result<()> {
         .context("this is not a valid WEBVTT file")?;
 
     let mut writer = csv::Writer::from_writer(io::stdout());
-    writer.write_record(&["id", "start", "end", "text"])?;
+    writer.write_record(["id", "start", "end", "text"])?;
 
-    for line in content.split("\r\n\r\n") {
+    for (i, line) in content.split("\r\n\r\n").enumerate() {
         if line.trim().is_empty() {
             break;
         }
 
-        let (id, start, end, text) = extract_line_infos(line)?;
-        writer.write_record(&[id, &start.to_string(), &end.to_string(), text])?;
+        let (id, start, end, text) = extract_line_infos(line)
+            .with_context(|| format!("an error occured on the line `{line:?}` around line {i}"))?;
+        writer.write_record([id, &start.to_string(), &end.to_string(), text])?;
     }
 
     writer.flush()?;
@@ -30,9 +31,11 @@ fn extract_line_infos(line: &str) -> anyhow::Result<(&str, u64, u64, &str)> {
     let (id, line) = line.split_once("\r\n").context("invalid line id format")?;
     let (start_end, text) = line
         .split_once("\r\n")
+        .context("cannot split by \\r\\n")
         .context("invalid line start-end format")?;
     let (start, end) = start_end
         .split_once(" --> ")
+        .context("cannot split by arrow (` --> `)")
         .context("invalid line start-end format")?;
 
     let start_milliseconds =
@@ -43,9 +46,9 @@ fn extract_line_infos(line: &str) -> anyhow::Result<(&str, u64, u64, &str)> {
 }
 
 fn convert_timer_in_milliseconds(timer: &str) -> Option<u64> {
-    let (hours, timer) = timer.split_once(":")?;
-    let (minutes, timer) = timer.split_once(":")?;
-    let (seconds, milliseconds) = timer.split_once(".")?;
+    let (hours, timer) = timer.split_once(':')?;
+    let (minutes, timer) = timer.split_once(':')?;
+    let (seconds, milliseconds) = timer.split_once('.')?;
 
     let hours: u64 = hours.parse().ok()?;
     let minutes: u64 = minutes.parse().ok()?;
@@ -63,11 +66,11 @@ mod tests {
     fn convertions() {
         assert_eq!(
             convert_timer_in_milliseconds("01:09:33.252"),
-            Some(1 * 60 * 60 * 1000 + 9 * 60 * 1000 + 33 * 1000 + 252)
+            Some(60 * 60 * 1000 + 9 * 60 * 1000 + 33 * 1000 + 252)
         );
         assert_eq!(
             convert_timer_in_milliseconds("01:09:36.547"),
-            Some(1 * 60 * 60 * 1000 + 9 * 60 * 1000 + 36 * 1000 + 547)
+            Some(60 * 60 * 1000 + 9 * 60 * 1000 + 36 * 1000 + 547)
         );
     }
 
